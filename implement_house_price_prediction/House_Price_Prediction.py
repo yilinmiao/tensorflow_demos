d8d553429b16127acde5225501bbc6fd3d16d27e
yilinmiao <yi.matheson@gmail.com>
2018-04-03 23:08:50 -0700

    Training data/testing data/learned regression

--- implement_house_price_prediction/House_Price_Prediction.py
+++ implement_house_price_prediction/House_Price_Prediction.py
@@ -27,31 +27,31 @@ def normalize(array):
     return (array - array.mean()) / array.std()
 
 # 1. Prepared Data
-# define number of training samples. 0.7 = 70%. We can take the first 70% since the values are randomized
+# Define number of training samples. 0.7 = 70%. We can take the first 70% since the values are randomized
 num_train_samples = math.floor(num_house * 0.7)
 
-# define training data
+# Define training data
 train_house_size = np.asarray(house_size[:num_train_samples])
 train_price = np.asanyarray(house_price[:num_train_samples])
 
 train_house_size_norm = normalize(train_house_size)
 train_price_norm = normalize(train_price)
 
-# define test data
+# Define test data
 test_house_size = np.asarray(house_size[num_train_samples:])
-test_price = np.asanyarray(house_price[num_train_samples:])
+test_house_price = np.asanyarray(house_price[num_train_samples:])
 
 test_house_size_norm = normalize(test_house_size)
-test_price_norm = normalize(test_price)
+test_house_price_norm = normalize(test_house_price)
 
 # Set up the TensorFlow placeholder that get updated as we descend down the gradient
-tf_house_size = tf.placeholder("float", name="house_size")
-tf_price = tf.placeholder("float", name="price")
+tf_house_size = tf.placeholder(tf.float32, name='house_size')
+tf_price = tf.placeholder(tf.float32, name='price')
 
 # Define the variables holding the size_factor and price we set during training.
 # We intialize them to some random values based on the normal distribution.
-tf_size_factor = tf.Variable(np.random.randn(), name="size_factor")
-tf_price_offset = tf.Variable(np.random.randn(), name="price_offset")
+tf_size_factor = tf.Variable(np.random.randn(), name='size_factor')
+tf_price_offset = tf.Variable(np.random.randn(), name='price_offset')
 
 # 2. Inference
 # Define the operations for the predicting values
@@ -62,7 +62,7 @@ tf_price_pred = tf.add(tf.multiply(tf_size_factor, tf_house_size), tf_price_offs
 
 # 3. Loss Measurement
 # Define the Loss Fuction(how much error) - Mean squared error
-tf_cost = tf.reduce_sum(tf.pow(tf_price_pred-tf_price, 2))/(2*num_train_samples)
+tf_cost = tf.reduce_sum(tf.pow(tf_price_pred - tf_price, 2)) /(2 * num_train_samples)
 
 # Optimizer learning rate. The size of the steps down the gradient
 learning_rate = 0.1
@@ -93,8 +93,7 @@ with tf.Session() as sess:
         if (iteration + 1) % display_every == 0:
             c = sess.run(tf_cost, feed_dict={tf_house_size: train_house_size_norm, tf_price: train_price_norm})
             print("iteration #:", '%04d' % (iteration + 1), "cost=", "{:.9f}".format(c), \
-                "size_factor=", sess.run(tf_size_factor), "price_offset=", sess.run(tf_price_offset))
-            
+                "size_factor=", sess.run(tf_size_factor), "price_offset=", sess.run(tf_price_offset))         
 
     print("Optimization Finished!")
     training_cost = sess.run(tf_cost, feed_dict={tf_house_size: train_house_size_norm, tf_price: train_price_norm})
@@ -102,7 +101,7 @@ with tf.Session() as sess:
 
     # Plot of the training and test data, and learned regression
 
-    Get values used to normalized data so we can denormalize data back to its original scale
+    # Get values used to normalized data so we can denormalize data back to its original scale
     train_house_size_mean = train_house_size.mean()
     train_house_size_std = train_house_size.std()
 
@@ -110,14 +109,14 @@ with tf.Session() as sess:
     train_price_std = train_price.std()
 
     # Plot the graph
-    plt.rcParams["figure.figsize"] = (10, 8)
+    plt.rcParams["figure.figsize"] = (10,8)
     plt.figure()
     plt.ylabel('Price')
     plt.xlabel('Size (sq.ft)')
     plt.plot(train_house_size, train_price, 'go', label='Training data')
-    plt.plot(test_house_size, test_price, 'mo', label='Testing data')
+    plt.plot(test_house_size, test_house_price, 'mo', label='Testing data')
     plt.plot(train_house_size_norm * train_house_size_std + train_house_size_mean,
-            (sess.run(tf_size_factor) * train_house_size_norm + sess.run(tf_price_offset) * train_price_std + train_price_mean,
+            (sess.run(tf_size_factor) * train_house_size_norm + sess.run(tf_price_offset)) * train_price_std + train_price_mean,
             label='Learned Regression')
 
     plt.legend(loc='upper left')
